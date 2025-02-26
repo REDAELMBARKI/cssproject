88e5235b7bf98678214549a6b3ad97354ce7decc
IREEL <redaelmbarki01@iCloud.com>
2025-02-26 18:10:28 +0000

    c3

--- cssbootstrap/tailwind.config.js
+++ cssbootstrap/tailwind.config.js
@@ -1,11 +1,13 @@
-
+/** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"
+    "./index.html",           // Ensure this points to your main HTML file
+    "./src/**/*.{js,ts,jsx,tsx}",  // If your JS/TS files are in the src folder
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('daisyui'),
+  ],
 }
